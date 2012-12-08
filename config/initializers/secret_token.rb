24f5992f7827f784a89aac0f0567a1e12c90d864
Matthew Haney <mahaney@q.com>
2012-12-07 22:25:27 -0700

    * Initial commit of bare repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RjfrcWeb::Application.config.secret_token = 'abdb0f2ed7e3d29110b3fffeb3257635545186e3cc2523ca948623af590be9b1edf603c095fccac558a4b6da15648b48d95354a6dcfaf66eb183cb4228bdd87d'
