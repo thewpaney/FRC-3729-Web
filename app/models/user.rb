f96562c3efbfa177c8fde9c6faba2c20d6f6eff3
root@azura <root@winterhold.skyrim.stratosphe.re>
2013-01-13 19:01:40 -0700

    * Moar changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,15 @@ class User < ActiveRecord::Base
   def self.authenticate(login, pass)
     where(pass: pass, email: login.downcase).first
   end
+
+  # Returns true if "admin" or "master" keys are present and true
+  def admin?
+    (self.credentials.has_key?(:master) and self.credentials[:master]) or (self.credentials.has_key?(:admin) and self.credentials[:admin])
+  end
+
+  def nice_credentials
+    "#{credentials.collect {|c, v| v ? c : nil}.delete_if {|c| c.nil? }.join(', ')}"
+  end
   
   belongs_to :blog_post
 
