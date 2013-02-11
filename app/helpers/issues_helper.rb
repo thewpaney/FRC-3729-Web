e205293faa61629ac7d65668232c27779a28c36c
Matthew Haney <mahaney@q.com>
2013-02-10 21:11:23 -0700

    * Lots of work on issues.

--- app/helpers/issues_helper.rb
+++ app/helpers/issues_helper.rb
@@ -1,2 +1,17 @@
 module IssuesHelper
+  def index_issue_table
+    out = ""
+    done = []
+    queue.each do |issue|
+      unless done.include?(issue)
+        out << "<tr>#{issue.id} - #{issue.name}</tr>"
+        subtasks = Issue.where(parent: issue.id)
+        subtasks.each do |s|
+          puts "----#{s.id} - #{s.name}"
+          done << s
+        end
+      end
+    end
+    
+  end
 end
--- db/migrate/20130119192138_create_issues.rb
+++ db/migrate/20130119192138_create_issues.rb
@@ -2,7 +2,10 @@ class CreateIssues < ActiveRecord::Migration
   def change
     create_table :issues do |t|
       t.string :description
-
+      t.string :name
+      t.text :watchers # Array of Users?
+      t.integer :parent
+      t.text :subtasks # Array of integers
       t.timestamps
     end
   end
